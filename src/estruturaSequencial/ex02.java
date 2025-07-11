998b9a8e7e5d0ece85b5d6e50663efaafd1e3bf0
Potyfael <potiguarrafa@gmail.com>
2025-07-11 01:40:09 -0300

    exercicio 3 resolvido e destrocados

--- src/estruturaSequencial/ex02.java
+++ src/estruturaSequencial/ex02.java
@@ -1,20 +1,19 @@
 package estruturaSequencial;
 
+//        Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+//        casas decimais conforme exemplos.
+//        Fórmula da área: area = π . raio2
+//        Considere o valor de π = 3.14159
+
 import java.util.Scanner;
 
-//Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
-    //de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
 public class ex02 {
-        public static void main(String[] args) {
-            System.out.print("Digite quatro valores inteiros: ");
-            Scanner sc = new Scanner(System.in);
-            int n1, n2, n3, n4, dif;
-            n1 = sc.nextInt();
-            n2 = sc.nextInt();
-            n3 = sc.nextInt();
-            n4 = sc.nextInt();
-            dif = n1 * n2 - n3 * n4;
-            sc.close();
-            System.out.printf("A diferença é : %d%n", dif);
-        }
+    public static void main(String[] args) {
+        System.out.print("Digite o valor do raio: ");
+        Scanner sc = new Scanner(System.in);
+        double area, raio = sc.nextDouble();
+        area = 3.14159 * (raio * raio);
+        System.out.printf("Área = %.5f%n", area);
+        sc.close();
+    }
 }
