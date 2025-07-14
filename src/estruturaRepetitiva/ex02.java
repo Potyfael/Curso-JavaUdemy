3aca1327d6d122fcd24da1cf2d91752c39fbc0fe
Potyfael <potiguarrafa@gmail.com>
2025-07-14 00:33:55 -0300

    ex02 resolvido

---
+++ src/estruturaRepetitiva/ex02.java
@@ -0,0 +1,29 @@
+package estruturaRepetitiva;
+
+import java.util.Scanner;
+
+public class ex02 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x, y;
+        while (true) {
+            System.out.print("Digite o valor de x e y respectivamente: ");
+            x = sc.nextInt();
+            y = sc.nextInt();
+            if (x > 0 && y > 0) {
+                System.out.println("Primeiro");
+            } else if (x > 0 && y < 0) {
+                System.out.println("Quarto");
+            } else if (x < 0 && y > 0) {
+                System.out.println("Segundo");
+            } else if (x < 0 & y < 0) {
+                System.out.println("Terceiro");
+            } else {
+                break;
+            }
+        }
+        sc.close();
+    }
+}
